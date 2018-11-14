667ba74a60dd39f6b3f1037e809b282e19c86581
CarelS072 <carelstarreveld@hotmail.com>
2018-11-15 11:33:04 +1300

    working product, needs refactoring

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -7,8 +7,7 @@ class App extends React.Component {
     weather: {
       apiK: '6632adbe463749a3ad3221126181311',
       // url: 'http://api.apixu.com/v1/current.json?key=' + this.state.weather.apiK + '&q=' + this.state.network.location,
-      urlF: 'http://api.apixu.com/v1/forecast.json?key=6632adbe463749a3ad3221126181311&q=Paris',
-      location: '',
+      urlF: '',
       tempInC: null,
       temptomorrow: null,
       mintempTomorrow: null
@@ -20,50 +19,66 @@ class App extends React.Component {
       urlLocation : '',
       ip: '',
       location: ''
-
       }
     }
   componentDidMount() {
    this.findIP()
-   this.callWeather()
   }
 
   findIP() {
     request
       .get(this.state.network.urlIP)
       .then( res => {
-        this.setState = ({
+        this.setState({
           network: {
-            ip: res.ip,
-            urlLocation: 'https://geo.ipify.org/api/v1?apiKey='+ this.state.network.apiK + '&ipAddress=' + this.state.network.ip
+            ip: res.body.ip,
+            urlLocation: 'https://geo.ipify.org/api/v1?apiKey='+ this.state.network.apiK + '&ipAddress=' + res.body.ip
           }
         })
+
+    request
       .get(this.state.network.urlLocation)
       .then( res => {
         this.setState({
+          // ...this.state,
+          // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+          // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/rest_parameters
+          // 
           network:  {
-            location: res.body.location.city
+            location: res.body.location.city,
+            ip: this.state.network.ip
           },
           weather: {
-            url: 'http://api.apixu.com/v1/current.json?key=' + this.state.weather.apiK + '&q=' + this.state.network.location,
+            urlF: `http://api.apixu.com/v1/forecast.json?key=${this.state.weather.apiK}&q=${res.body.location.city}`,
+            // urlF: 'http://api.apixu.com/v1/forecast.json?key=6632adbe463749a3ad3221126181311&q=Paris'
           }
         })
+        this.callWeather()
+
       })
-      })
+    })
   }
 
   callWeather() {
-    console.log('weather has been called')
+    // console.log('weather has been called'+ this.state.weather.urlF)
+    let weatherURL = this.state.weather.urlF
     request
-    .get(this.state.weather.url)
+    .get(weatherURL)
     .then(res => {
       this.setState({
         weather: { 
-          location: res.body.location.name,
           tempInC: res.body.current.temp_c,
           temptomorrow: res.body.forecast.forecastday['0'].day.maxtemp_c,
           mintempTomorrow: res.body.forecast.forecastday['0'].day.mintemp_c
-        }
+          }
+        //   ,
+        //   network:  {
+        //     location: this.state.network.location,
+        //     apiK: this.state.network.apiK,
+        //     urlIP: this.state.network.urlIP,
+        //     urlLocation : this.state.network.urlLocation,
+        //     ip: this.state.network.ip
+        // }
       })
     })
   }
@@ -75,15 +90,11 @@ class App extends React.Component {
       <h1>Hello API!</h1>
         <h5>my old friend</h5>
       <div>
-        <p>The temp is : {this.state.weather.tempInC}</p>
-        <p>Tomorrow's temp is : {this.state.weather.temptomorrow}</p>
-        <p>Tomorrow's minimum temp is : {this.state.weather.mintempTomorrow}</p>
+        <p>The temp is : {this.state.weather.tempInC} °C</p>
+        <p>Tomorrow's temp is : {this.state.weather.temptomorrow} °C</p>
+        <p>Tomorrow's minimum temp is : {this.state.weather.mintempTomorrow} °C</p>
         <p>The city I am located it : {this.state.network.location}</p>
         <p>IP  : {this.state.network.ip}</p>
-        
-
-
-
       </div>
     </React.Fragment>
     
